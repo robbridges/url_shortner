c590ffda97bde5c900811e1f3c0bfdcf5666b097
Rob Bridges <Rmb101187@gmail.com>
2023-11-13 18:13:34 -0800

    Integration test for insert url complete, going to dockerize app and put it on aws

--- cmd/api/handlers_integration_test.go
+++ cmd/api/handlers_integration_test.go
@@ -1,14 +1,73 @@
 package main
 
 import (
+	"bytes"
 	"echo_url_shortner/data"
 	"echo_url_shortner/models"
+	"encoding/json"
 	"github.com/labstack/echo/v4"
 	"github.com/stretchr/testify/assert"
 	"net/http"
 	"net/http/httptest"
 	"testing"
 )
+
+func TestApp_InsertUrl(t *testing.T) {
+	e := echo.New()
+
+	cfg := data.DefaultTestPostgresConfig()
+	db, err := data.Open(cfg)
+	if err != nil {
+		t.Errorf("Expected no error, but got %s", err)
+	}
+	urlService := models.UrlService{DB: db}
+
+	app := App{
+		UrlModel: &urlService,
+	}
+	t.Run("Happy path", func(t *testing.T) {
+		payload := new(bytes.Buffer)
+		json.NewEncoder(payload).Encode(map[string]string{
+			"url": "https://testme.com",
+		})
+		req := httptest.NewRequest(http.MethodPost, "/", payload)
+		req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
+
+		rec := httptest.NewRecorder()
+
+		c := e.NewContext(req, rec)
+
+		if assert.NoError(t, app.InsertUrl(c)) {
+			assert.Equal(t, http.StatusOK, rec.Code)
+
+			var url models.Url
+			if assert.NoError(t, json.NewDecoder(rec.Body).Decode(&url)) {
+				assert.Equal(t, "https://testme.com", url.Url)
+			}
+			err = urlService.DeleteUrl(url.ShortUrl)
+			if err != nil {
+				t.Errorf("Expected no error, but got %s", err)
+			}
+		}
+	})
+	t.Run("Duplicate url", func(t *testing.T) {
+		payload := new(bytes.Buffer)
+		json.NewEncoder(payload).Encode(map[string]string{
+			"url": "https://www.google.com",
+		})
+		req := httptest.NewRequest(http.MethodPost, "/", payload)
+		req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
+
+		rec := httptest.NewRecorder()
+
+		c := e.NewContext(req, rec)
+		_ = app.InsertUrl(c)
+		if assert.NoError(t, app.InsertUrl(c)) {
+			assert.Equal(t, http.StatusConflict, rec.Code)
+		}
+	})
+
+}
 
 func TestAppIntegration_GetUrl(t *testing.T) {
 	e := echo.New()
